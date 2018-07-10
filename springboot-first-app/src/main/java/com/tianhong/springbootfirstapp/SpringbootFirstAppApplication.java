8cde8c0c561521519455b53d4bb14a69e23b4eb4
tianhong <406507715@qq.com>
2018-07-10 19:54:04 +0800

    first commit

---
+++ springboot-first-app/src/main/java/com/tianhong/springbootfirstapp/SpringbootFirstAppApplication.java
@@ -0,0 +1,12 @@
+package com.tianhong.springbootfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFirstAppApplication.class, args);
+    }
+}
---
+++ springboot-first-app/src/test/java/com/tianhong/springbootfirstapp/SpringbootFirstAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tianhong.springbootfirstapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootFirstAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

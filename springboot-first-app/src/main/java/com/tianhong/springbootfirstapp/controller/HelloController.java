d8364c2bf3fc3bcf01c57e3fe33220a1a8a5489c
tianhong <406507715@qq.com>
2018-07-11 14:39:12 +0800

    Value and ConfigurationProperties annotation

--- springboot-first-app/src/main/java/com/tianhong/springbootfirstapp/controller/HelloController.java
+++ springboot-first-app/src/main/java/com/tianhong/springbootfirstapp/controller/HelloController.java
@@ -1,12 +1,24 @@
 package com.tianhong.springbootfirstapp.controller;
 
+import com.tianhong.springbootfirstapp.dao.ConfigBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${my.name}")
+    private String name;
+
+    @Value("${my.age}")
+    private int age;
+
+    @Autowired
+    ConfigBean configBean;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return configBean.toString();
     }
 }
---
+++ springboot-first-app/src/main/java/com/tianhong/springbootfirstapp/dao/ConfigBean.java
@@ -0,0 +1,42 @@
+package com.tianhong.springbootfirstapp.dao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "my")
+//可以不加component
+@Component
+public class ConfigBean {
+    private String name;
+    private int age;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName()+" "+this.getAge() +" "+this.getNumber();
+    }
+}
